cad2f713778964e41729cb06ccd5b46b32e3db39
Caio Yago <Caioyagovilela@hotmail.com>
2024-10-29 14:20:54 -0300

    Add files via upload

---
+++ Kjuma/03Projeto-Kjuma-FrontEnd-Responsivo-main/js/pizzas.js
@@ -0,0 +1,56 @@
+let pizzaJson = [
+  {
+    id: 1,
+    name: "Batom Depilador Facial Indolor",
+    img: "../../img/batom6.jpg",
+    price: [20.0, 23.0, 25.0],
+    sizes: ["", "", "12 fatias"],
+    description:
+      "O Batom Depilador Elétrico é portátil e discreto, em forma de batom.",
+  },
+  {
+    id: 2,
+    name: "Calabresa",
+    img: "images/pizza2.png",
+    price: [21.0, 24.0, 26.0],
+    sizes: ["6 fatias", "8 fatias", "12 fatias"],
+    description:
+      "Molho de tomate, mussarela, calabresa fatiada, cebola e orégano",
+  },
+  {
+    id: 3,
+    name: "Quatro Queijos",
+    img: "images/pizza3.png",
+    price: [23.0, 26.0, 28.0],
+    sizes: ["6 fatias", "8 fatias", "12 fatias"],
+    description:
+      "Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano",
+  },
+  {
+    id: 4,
+    name: "Brasileira",
+    img: "images/pizza4.png",
+    price: [25.0, 28.0, 30.0],
+    sizes: ["6 fatias", "8 fatias", "12 fatias"],
+    description:
+      "Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano",
+  },
+  {
+    id: 5,
+    name: "Portuguesa",
+    img: "images/pizza5.png",
+    price: [24.0, 27.0, 29.0],
+    sizes: ["6 fatias", "8 fatias", "12 fatias"],
+    description:
+      "Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano",
+  },
+  {
+    id: 6,
+    name: "Moda da Casa",
+    img: "images/pizza6.png",
+    price: [30.0, 33.0, 35.0],
+    sizes: ["6 fatias", "8 fatias", "12 fatias"],
+    description:
+      "Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano",
+  },
+];
